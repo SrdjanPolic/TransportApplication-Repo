b33560e737f456212c28c7c5b84b1d8defa6f4d6
Aleksandar Dragojević <adragojevic4094@cosmoconsult.com>
2019-03-02 23:02:45 +0100

    extend tables and fields

--- DBLayerPOC/Models/Entity.cs
+++ DBLayerPOC/Models/Entity.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace DBLayerPOC.Models
 {
+    /// <summary>
+    /// Comment this line of code..
+    /// TODO - Define table relations
+    /// </summary>
     public class Entity
     {
         public int Id { get; set; }
