859ada2682ca59e604eb6cc98881df49a252f248
Aleksandar Dragojević <adragojevic4094@cosmoconsult.com>
2019-03-10 16:23:10 +0100

    Angular Project added

---
+++ AngularClientApp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClient } from 'selenium-webdriver/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClient,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
