82d21c3c4425ba86863bc160ef0ef9c124d8610f
SrdjanPolic <srdjan.polic@yahoo.com>
2019-09-06 22:51:27 +0200

    USer Fields Extension

--- DBLayerPOC/Infrastructure/Login/LoginModel.cs
+++ DBLayerPOC/Infrastructure/Login/LoginModel.cs
@@ -8,7 +8,10 @@ namespace DBLayerPOC.Infrastructure.Login
 {
     public class LoginModel : Entity
     {
+        public string Name { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public bool IsInactive { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
