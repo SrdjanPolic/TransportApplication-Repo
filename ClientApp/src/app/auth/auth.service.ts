c71e1d18dc7c9ace6485a598564486e8591bc431
Aleksandar Dragojevic <adragojevic@gmail.com>
2019-09-08 17:39:08 +0200

    Angular Login

--- ClientApp/src/app/auth/auth.service.ts
+++ ClientApp/src/app/auth/auth.service.ts
@@ -2,25 +2,47 @@ import { Injectable } from '@angular/core';
 import {Router} from '@angular/router';
 import {BehaviorSubject} from 'rxjs';
 import {User} from './user.model';
+import {RepositoryService} from './../shared/repository.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private loggedIn = new BehaviorSubject<boolean>(false);
-
-  get isLoggedIn() {
+    get isLoggedIn() {
     return this.loggedIn.asObservable();
   }
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private repository: RepositoryService, private http: HttpClient) { }
 
   login(user: User) {
-    if (user.username !== '' && user.password !== '') {            //dodati logiku za proveru korisnika
-      this.loggedIn.next(true);
-      this.router.navigate(['/home']);
+    let routeToLogin: string;
+    routeToLogin = environment.urlAddress + '/' + 'Auth';
+    let credentials = JSON.stringify(user);
+    this.http.post(routeToLogin, credentials, {
+      headers: new HttpHeaders ({
+        "Content-Type" : "application/json"
+      })
+      }).subscribe(response => {
+        let token = (<any>response).token;
+        localStorage.setItem("jwt",token);
+        this.loggedIn.next(true);
+        this.router.navigate(['/home']);
+      }, err => {
+        this.loggedIn.next(false);
+      });
     }
-  }
+
+
+  //   if (user.username !== '' && user.password !== '') {            //dodati logiku za proveru korisnika
+  //     this.loggedIn.next(true);
+  //     this.router.navigate(['/home']);
+  //   }
+  // }
+
   logout() {
     this.loggedIn.next(false);
     this.router.navigate(['/login']);
