6d8035de0403860bb087a938590c240dd47dbbf7
SrdjanPolic <srdjan.polic@yahoo.com>
2019-10-06 16:49:17 +0200

    Added logging and IsAdmin info in response

--- TransportWebAPI/Controllers/Authentification/AuthController.cs
+++ TransportWebAPI/Controllers/Authentification/AuthController.cs
@@ -9,6 +9,7 @@ using DBLayerPOC.Infrastructure;
 using DBLayerPOC.Infrastructure.Login;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Service.Data;
 
@@ -16,13 +17,16 @@ namespace TransportWebAPI.Controllers.Authentification
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController : Controller
     {
         private IUnitOfWork<AppDbContext> _unitOfWork;
 
-        public AuthController(IUnitOfWork<AppDbContext> unitOfWork)
+        private readonly ILogger _logger;
+
+        public AuthController(IUnitOfWork<AppDbContext> unitOfWork, ILogger<AuthController> logger)
         {
             _unitOfWork = unitOfWork;
+            _logger = logger;
         }
 
 
@@ -35,27 +39,37 @@ namespace TransportWebAPI.Controllers.Authentification
                 return BadRequest("Invalid client request");
             }
 
-            var users = _unitOfWork.GetRepository<LoginModel>().
-                    GetList().Items.ToList();
-            if(users.Any(x => x.Password.Equals(user.Password) && x.Username.Equals(user.Username)))
+            try
             {
-                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
-                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var users = _unitOfWork.GetRepository<LoginModel>().
+                        GetList().Items.ToList();
+                if (users.Any(x => x.Password.Equals(user.Password) && x.Username.Equals(user.Username)))
+                {
+                    var loggedUser = users.FirstOrDefault(x => x.Password.Equals(user.Password) && x.Username.Equals(user.Username));
+                    var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+                    var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
-                var tokeOptions = new JwtSecurityToken(
-                    issuer: "http://localhost:56515",
-                    audience: "http://localhost:56515",
-                    claims: new List<Claim>(),
-                    expires: DateTime.Now.AddMinutes(15),
-                    signingCredentials: signinCredentials
-                );
+                    var tokeOptions = new JwtSecurityToken(
+                        issuer: "http://localhost:56515",
+                        audience: "http://localhost:56515",
+                        claims: new List<Claim>(),
+                        expires: DateTime.Now.AddMinutes(15),
+                        signingCredentials: signinCredentials
+                    );
 
-                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
-                return Ok(new { Token = tokenString });
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                    _logger.LogError("sve OK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    return Ok(new { Token = tokenString, loggedUser.IsAdmin });
+                }
+                else
+                {
+                    return Unauthorized();
+                }
             }
-            else
+            catch(Exception e)
             {
-                return Unauthorized();
+                _logger.LogError(e.Message);
+                throw;
             }
         }
     }
