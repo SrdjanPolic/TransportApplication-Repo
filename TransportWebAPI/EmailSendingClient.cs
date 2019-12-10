6e5d0ab8703799e8670fa089dd9cff9f8959fdb8
SrdjanPolic <srdjan.polic@yahoo.com>
2019-12-10 22:21:57 +0100

    Email log

---
+++ TransportWebAPI/EmailSendingClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace TransportWebAPI
+{
+    public interface IEmailSendingClient
+    {
+       void SendLogEmail(string logMessage);
+    }
+
+    public class EmailSendingClient
+    {
+        private SmtpClient smtpClient;
+        public EmailSendingClient()
+        {
+            smtpClient = new SmtpClient
+            {
+                Host = "smtp.gmail.com", // set your SMTP server name here
+                Port = 587, // Port 
+                EnableSsl = true,
+                Credentials = new NetworkCredential("atomicspedlog@gmail.com", "atomic1234")
+            };
+        }
+
+        public async void SendLogEmail(string logMessage)
+        {
+            using (var message = new MailMessage("atomicspedlog@gmail.com", "atomicspedlog@mail.com")
+            {
+                Subject = "Atomic Sped Error",
+                Body = logMessage
+            })
+            {
+                await smtpClient.SendMailAsync(message);
+            }
+        }
+    }
+}
