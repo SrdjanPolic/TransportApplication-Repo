44af991c63612ca5eba615dde8bf4824384a75cd
Aleksandar Dragojevic <adragojevic@gmail.com>
2020-04-14 06:59:01 +0200

    ASP.net core - Sales & Purchase Invoice Dates modified

--- TransportWebAPI/Controllers/SalesInvoiceHeadersController.cs
+++ TransportWebAPI/Controllers/SalesInvoiceHeadersController.cs
@@ -131,7 +131,7 @@ namespace TransportWebAPI.Controllers
                 if (property.PropertyType == typeof(DateTime) || property.PropertyType == typeof(DateTime?) && property.Name != "LastChangeDateTime")
                 {
                     var datum = property.GetValue(header, null);
-                    if(((DateTime)datum).Hour == 23 && ((DateTime)datum).Minute == 0 && ((DateTime)datum).Second == 0)
+                    if((((DateTime)datum).Hour == 23 || ((DateTime)datum).Hour == 22) && ((DateTime)datum).Minute == 0 && ((DateTime)datum).Second == 0)
                     {
                         property.SetValue(header, ((DateTime)property.GetValue(header, null)).AddHours(2), null);
                     }
