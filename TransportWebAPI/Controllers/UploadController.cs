0f2e000b5a5a832035c98d39c4d2cbdde02c290b
SrdjanPolic <srdjan.polic@yahoo.com>
2021-04-17 21:33:47 +0200

    File upload inital changes

--- TransportWebAPI/Controllers/UploadController.cs
+++ TransportWebAPI/Controllers/UploadController.cs
@@ -1,82 +1,80 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using System.IO;
-//using System.Net.Http.Headers;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Http.Headers;
 
-//namespace TransportWebAPI.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class UploadController : ControllerBase
-//    {
-//        [HttpPost, DisableRequestSizeLimit]
-        
+namespace TransportWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost, DisableRequestSizeLimit]
+        public IActionResult Upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = Path.Combine("Resources", "Docs");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
 
-//        public IActionResult Upload()
-//        {
-//            try
-//            {
-//                var file = Request.Form.Files[0];
-//                var folderName = Path.Combine("Resources", "Docs");
-//                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
 
-//                if (file.Length > 0)
-//                {
-//                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-//                    var fullPath = Path.Combine(pathToSave, fileName);
-//                    var dbPath = Path.Combine(folderName, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    return Ok(new { dbPath });
 
-//                    using (var stream = new FileStream(fullPath, FileMode.Create))
-//                    {
-//                        file.CopyTo(stream);
-//                    }
-//                    return Ok(new { dbPath });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+        //// GET: api/Upload
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
-//                }
-//                else
-//                {
-//                    return BadRequest();
-//                }
-//            }
-//            catch (Exception ex)
-//            {
-//                return StatusCode(500, $"Internal server error: {ex}");
-//            }
-//        }
-//        //// GET: api/Upload
-//        //[HttpGet]
-//        //public IEnumerable<string> Get()
-//        //{
-//        //    return new string[] { "value1", "value2" };
-//        //}
+        //// GET: api/Upload/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
-//        //// GET: api/Upload/5
-//        //[HttpGet("{id}", Name = "Get")]
-//        //public string Get(int id)
-//        //{
-//        //    return "value";
-//        //}
+        //// POST: api/Upload
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
 
-//        //// POST: api/Upload
-//        //[HttpPost]
-//        //public void Post([FromBody] string value)
-//        //{
-//        //}
+        //// PUT: api/Upload/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
-//        //// PUT: api/Upload/5
-//        //[HttpPut("{id}")]
-//        //public void Put(int id, [FromBody] string value)
-//        //{
-//        //}
-
-//        //// DELETE: api/ApiWithActions/5
-//        //[HttpDelete("{id}")]
-//        //public void Delete(int id)
-//        //{
-//        //}
-//    }
-//}
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
